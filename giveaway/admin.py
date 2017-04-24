bdba576ace0b8f8812acb346ba630a2246ed8985
Tyler Canfield <tylerjoncanfield@gmail.com>
2017-04-23 21:40:36 -0600

    Changed database, trying to add images

--- giveaway/admin.py
+++ giveaway/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Prize
+from .models import Category, Prize, Giveaway
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Prize)
+admin.site.register(Giveaway)
--- giveaway/models.py
+++ giveaway/models.py
@@ -1,19 +1,44 @@
 from django.db import models
 import datetime
 # Create your models here.
-
-class Prize(models.Model):
+class Category(models.Model):
+    parent_category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
     name = models.TextField()
-    start_date = models.DateTimeField(default=datetime.datetime.now)
-    end_date = models.DateTimeField(blank=True, null=True)
-    odds = models.DecimalField(max_digits=999, decimal_places=999)
-    item_link = models.TextField()
-    referral_link = models.TextField()
-    value = models.DecimalField(max_digits=999, decimal_places=2)
-
-    def end(self):
-        self.end_date = datetime.datetime.now()
-        self.save()
+    description = models.TextField()
 
     def __str__(self):
         return self.name
+
+class Prize(models.Model):
+    name = models.TextField()
+    image = models.ImageField(upload_to = 'static/images')
+    description = models.TextField()
+    link = models.URLField()
+    referral_link = models.URLField()
+    value = models.DecimalField(max_digits=999, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class Giveaway(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    prize = models.ForeignKey(Prize, on_delete=models.CASCADE)
+    start_time = models.DateTimeField(default=datetime.datetime.now)
+    end_time = models.DateTimeField(blank=True, null=True)
+    odds = models.DecimalField(max_digits=999, decimal_places=999)
+    winner_email_address = models.EmailField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    attempts = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def start(self):
+        self.start = datetime.datetime.now()
+        self.save()
+
+    def end(self):
+        self.end = datetime.datetime.now()
+        # TODO: Code to handle sending the prize to the winner and any other alerts necessary to show that the giveaway has ended
+        self.save()
