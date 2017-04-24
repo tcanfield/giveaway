40d7be3504bc9146055ed8d36f6f3d66933a2f24
Tyler Canfield <tylerjoncanfield@gmail.com>
2017-04-23 18:23:48 -0600

    add list

--- giveaway/views.py
+++ giveaway/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Prize
 
 # Create your views here.
+
+def giveaway_list(request):
+    active_prizes = Prize.objects.filter(end_date=None);
+    return render(request, 'giveaway/giveaway_list.html', {'active_prizes': active_prizes})
