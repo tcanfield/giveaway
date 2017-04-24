ac9f0ffc4def0d5f8116d041fc7a48857f6d9e0f
Tyler Canfield <tylerjoncanfield@gmail.com>
2017-04-23 22:40:58 -0600

    Fixed images

--- giveaway/urls.py
+++ giveaway/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
 from . import views
 
 urlpatterns = [
     url(r'^$', views.giveaway_list, name='giveaway_list'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
