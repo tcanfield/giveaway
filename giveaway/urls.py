e44ed63bea2e3cc75e85844ff5d1ad2a4f3b0c35
Tyler Canfield <tylerjoncanfield@gmail.com>
2017-04-24 23:26:06 -0600

    fix url

--- giveaway/urls.py
+++ giveaway/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.category_list, name='category_list'),
-    url(r'^/giveaway_list/(?P<pk>\d+)/$', views.giveaway_list, name='giveaway_list')
+    url(r'^giveaway_list/(?P<pk>\d+)/$', views.giveaway_list, name='giveaway_list')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
