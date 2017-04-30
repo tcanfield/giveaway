fc397739006903dc1d11310a43d067c5f33d463d
Tyler Canfield <tylerjoncanfield@gmail.com>
2017-04-30 14:02:58 -0600

    add form to enter giveaway

--- giveaway/admin.py
+++ giveaway/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Prize, Giveaway
+from .models import Category, Prize, Giveaway, Entry
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Prize)
 admin.site.register(Giveaway)
+admin.site.register(Entry)
